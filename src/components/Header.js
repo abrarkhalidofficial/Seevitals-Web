4976d96400add7127db4fbff0952efdef863b9ed
ibilalchaudhary <bilalchaudhary478@gmail.com>
2021-12-29 17:09:15 +0500

    header fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
 import React from "react";
+import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
     <div className="header">
       <div className="header__wrapper">
-        <img
-          src="https://avatars.githubusercontent.com/u/68416499?s=400&u=44f53c74beb2463c2f40ea9b2eda2cb3dd27c22e&v=4"
-          className="brand__logo"
-        />
+        <img src={logo} className="brand__logo" />
         <div className="nav">
           <div className="nav__link">
             <input
@@ -23,14 +21,6 @@ export default function Header() {
               className="nav__link__input"
               name="nav__link__input"
             />
-            <div className="nav__link__content">Products</div>
-          </div>
-          <div className="nav__link">
-            <input
-              type="radio"
-              className="nav__link__input"
-              name="nav__link__input"
-            />
             <div className="nav__link__content">About Us</div>
           </div>
           <div className="nav__link">
@@ -39,7 +29,15 @@ export default function Header() {
               className="nav__link__input"
               name="nav__link__input"
             />
-            <div className="nav__link__content">Contact</div>
+            <div className="nav__link__content">Products</div>
+          </div>
+          <div className="nav__link">
+            <input
+              type="radio"
+              className="nav__link__input"
+              name="nav__link__input"
+            />
+            <div className="nav__link__content">Contact Us</div>
           </div>
         </div>
       </div>
