ed2aa3d1430c55a4bc339140c58db2336f5daa5b
Arslan <73060834+muhammadarslan478@users.noreply.github.com>
2021-12-30 18:30:16 +0500

    story partially done

--- src/screens/AboutUs.js
+++ src/screens/AboutUs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import aboutus__background__left from "../assets/Aboutus__background__left.png";
 import aboutus__background__right from "../assets/Aboutus__background__right.png";
+import aboutus__story__background__img from "../assets/Aboutus__story__background__img.png";
 
 export default function AboutUs() {
   return (
@@ -25,26 +26,37 @@ export default function AboutUs() {
           Like You’ve Never Seen
         </div>
       </div>
-    </div>
-  );
-}
-
-{
-  /* <div className="about__hero__section__heading">
-          <div className="about__hero__section__heading1">
-            We Want to See you
+      <div className="about__story">
+        <img
+          src={aboutus__story__background__img}
+          className="about__story__img__bg"
+        />
+        <div className="aboutus__story__body">
+          <div className="aboutus__story__body1">
+            SeeVITALS Technologies is an ambitious project brought to life by 3
+            friends that have recognized the dire need of technological
+            interventions in the healthcare industry. 2 doctors having worked in
+            numerous healthcare setups from around the world understood that the
+            need to optimize the role of nurses and allied hospital staff
+            remained crucial. Especially in ICUs (Intensive Care Units) and HDUs
+            (High Dependency Units), where time was of the essence. While
+            shifting their focus from their individual clinical practices to a
+            solution best fit for both hospital staff and their patients, the
+            idea started taking shape during a mutual hospital management
+            degree.
           </div>
-          <div className="about__hero__section__heading2">
-            <span>Technology</span> in <span>Healthcare</span>
-          </div>
-          <div className="about__hero__section__heading3">
-            Like You’ve Never Seen
+          <div className="aboutus__story__body2">
+            We felt that the time taken for clerical work (patient charting,
+            hourly recordings, etc.) could be better utilized and patient care
+            can thus be improved. Here’s where our computer scientist came in.
+            With thorough experience in the field and an existing network with
+            the health industry alike, Dayyan proposed to introduce a technology
+            that could cater to the nurses and doctors as well as improve
+            patient risk assessment and bed throughput time.
           </div>
         </div>
-        <img
-          src={aboutus__background__right}
-          alt="about__background__rightimg"
-          className="about__hero__section__right__img"
-        />
-      </div> */
+        <div className="aboutus__story__heading">Our Story</div>
+      </div>
+    </div>
+  );
 }
