037cc73a6e860e4f9d9bdb22ebc6ad0640e84ac0
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-12-31 14:11:07 +0500

    firebase init and header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,91 @@
-import React from "react";
-import { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 
-export default class Header extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isScrolling: false,
-      isHeaderOpen: window.innerWidth > 1000,
-    };
-  }
-  componentDidMount() {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 0) {
-        this.setState({
-          isScrolling: true,
-        });
-      } else if (window.scrollY === 0) {
-        this.setState({
-          isScrolling: false,
-        });
+export default function Header() {
+  const [isHeaderOpen, setIsHeaderOpen] = useState(true);
+
+  useEffect(() => {
+    if (window.innerWidth > 800) {
+      setIsHeaderOpen(true);
+    } else {
+      setIsHeaderOpen(false);
+    }
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 800) {
+        setIsHeaderOpen(true);
+      } else {
+        setIsHeaderOpen(false);
       }
     });
-    window.addEventListener("resize", () => {
-      this.setState({
-        isHeaderOpen: window.innerWidth > 1000,
-      });
-    });
-  }
-  render() {
-    return (
-      <div className="header">
-        <div className="header__wrapper">
-          <Link to="/">
-            <img src={logo} className="brand__logo" />
-          </Link>
+  }, []);
+
+  const navigate = useNavigate();
+  return (
+    <div className="header">
+      <div className="header__wrapper">
+        <Link
+          to="/"
+          onClick={() => {
+            document.getElementById("home").checked =
+              !document.getElementById("home").checked;
+          }}
+        >
+          <img src={logo} className="brand__logo" />
+        </Link>
+        <button
+          className="header__mobile__btn"
+          onClick={() => {
+            isHeaderOpen ? setIsHeaderOpen(false) : setIsHeaderOpen(true);
+          }}
+        >
+          {isHeaderOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-x"
+            >
+              <line x1="18" y1="6" x2="6" y2="18"></line>
+              <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-menu"
+            >
+              <line x1="3" y1="12" x2="21" y2="12"></line>
+              <line x1="3" y1="6" x2="21" y2="6"></line>
+              <line x1="3" y1="18" x2="21" y2="18"></line>
+            </svg>
+          )}
+        </button>
+        {isHeaderOpen ? (
           <div className="nav">
             <div className="nav__link">
               <input
                 type="radio"
+                id="home"
                 className="nav__link__input"
                 name="nav__link__input"
+                defaultChecked={true}
+                onClick={() => {
+                  navigate("/");
+                }}
               />
               <div className="nav__link__content">Home</div>
             </div>
@@ -50,28 +94,37 @@ export default class Header extends Component {
                 type="radio"
                 className="nav__link__input"
                 name="nav__link__input"
+                onClick={() => {
+                  navigate("/about");
+                }}
               />
               <div className="nav__link__content">About Us</div>
             </div>
+            {/* <div className="nav__link">
+            <input
+              type="radio"
+              className="nav__link__input"
+              name="nav__link__input"
+              onClick={() => {
+                navigate("/product");
+              }}
+            />
+            <div className="nav__link__content">Products</div>
+          </div> */}
             <div className="nav__link">
               <input
                 type="radio"
                 className="nav__link__input"
                 name="nav__link__input"
-              />
-              <div className="nav__link__content">Products</div>
-            </div>
-            <div className="nav__link">
-              <input
-                type="radio"
-                className="nav__link__input"
-                name="nav__link__input"
+                onClick={() => {
+                  navigate("/contact");
+                }}
               />
               <div className="nav__link__content">Contact Us</div>
             </div>
           </div>
-        </div>
+        ) : null}
       </div>
-    );
-  }
+    </div>
+  );
 }
